731a3273f5d75dc9cac1b258391b42bef8426d95
danielkhait <danielkhait@gmail.com>
2022-06-24 16:59:05 +0300

    input validation using regex and switch

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,25 +1,57 @@
 #include "Calculator.h"
+#define  invalidInput 'C'
+
 
 using std::cout; using std::endl; using std::string; using std::cin;
 
 void  inputReceive()
 {
-	string userInput;
+	string val1, val2;
+	char op;
 
 	cout << "Hello!" << endl;
-	cout << "Please type 2 numbers and a desired operator between them." << endl;
-	cout << "Example: 2 + 5" << endl;
-	cout << "When finished hit enter." << endl;
+	cout << "Please enter an expression with the following foramt:" << endl;
+	cout << "Number [space] Operator [space] Number [space]" << endl;
 
-	cin >> userInput;
-	checkValidInput(userInput);
-
+	do { cin >> val1; } while (!checkIfNum(val1));
+	do { cin >> op; } while (!checkIfOperator(op));
+	do { cin >> val2; } while (!checkIfNum(val2));
 }
-void checkValidInput(string& uInput)
-{
 
+/*
+Regex used for validation,
+first: the '-' is optional,and then a digit (at least one)
+second: the '-' is optional and then a digit, followed by a period, and then a digita again
+
+cin -> ignores whitespaces
+*/
+bool checkIfNum(string& val)
+{
+	std::regex intFormat("-?\\d+");
+	std::regex floatFormat("-?\\d+\\.\\d+");
+	if (!std::regex_match(val, intFormat) && !std::regex_match(val, floatFormat))
+	{
+		cout << "\n The number you've entered is illegal" << endl;
+		return false;
+	}
+
+
+	return true;
 }
-bool checkValidDigit(char& digit)
+bool checkIfOperator(char& op)
 {
-
+	switch (op)
+	{
+	case '-':
+		return true;
+	case'+':
+		return true;
+	case '*':
+		return true;
+	case':':
+		return true;
+	default:
+		cout << "\n The operator you've entered is illegal" << endl;
+		return false;
+	}
 }
