d62b31119dfdf70d6d5a55b7f536a68a06594b26
danielkhait <danielkhait@gmail.com>
2022-06-25 19:53:29 +0300

    large numbers exception created

--- Calculator.cpp
+++ Calculator.cpp
@@ -9,26 +9,29 @@ void Calculator::inputReceive()
 	do { cin >> input_operator; } while (!Validator::isValidOperator(input_operator));
 	do { cin >> val2; } while (!Validator::isValidNumber(val2));
 	try { calculateResult(std::stol(val1), std::stol(val2), input_operator); }
-	catch (const char* msg) { cout << msg << endl; }
+	catch (std::exception) { cout << "Numbers are too large\n" << endl; }
 }
 
 void Calculator::calculateResult(const int& val1, const int& val2, char& input_operator)
 {
-	switch (input_operator) {
-	case '-':
-		cout << "The result is: " << (val1 - val2) << endl;
-		break;
-	case'+':
-		cout << "The result is: " << (val1 + val2) << endl;
-		break;
-	case '*':
-		cout << "The result is: " << (val1 * val2) << endl;
-		break;
-	case'/':
-		if (val2 == 0)
-			throw "\nDivision by Zero, illegal\n";
-		cout << "The result is: " << (val1 / val2) << endl;
-		break;
+	try{
+		switch (input_operator) {
+		case '-':
+			cout << "The result is: " << (val1 - val2) << endl;
+			break;
+		case'+':
+			cout << "The result is: " << (val1 + val2) << endl;
+			break;
+		case '*':
+			cout << "The result is: " << (val1 * val2) << endl;
+			break;
+		case'/':
+			if (val2 == 0)
+				throw "\nDivision by Zero, illegal\n";
+			cout << "The result is: " << (val1 / val2) << endl;
+			break;
+		}
 	}
+	catch (const char* msg) { cout << msg << endl; };
+	
 }
-
